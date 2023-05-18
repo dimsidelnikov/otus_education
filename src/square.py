168b1aec6e099f6dc95eeb6a868b95aa9352e898
dimsidelnikov <dim.sidelnikov@gmail.com>
2023-05-18 17:46:18 +0300

    changed the inheritance of the square class

--- src/square.py
+++ src/square.py
@@ -1,24 +1,7 @@
-from src.figure import Figure
+from src.rectangle import Rectangle
 
 
-class Square(Figure):
+class Square(Rectangle):
     def __init__(self, a_side):
+        super().__init__(a_side, a_side)
         self.name = 'Square'
-        self.a_side = a_side
-        self.check_possible_create_square(a_side)
-
-    @property
-    def perimeter(self):
-        return self.a_side * 4
-
-    @property
-    def area(self):
-        return self.a_side ** 2
-
-    @staticmethod
-    def check_possible_create_square(a_side):
-        if type(a_side) != int:
-            raise ValueError(f'Side must be integer. Got: {a_side}: {type(a_side)}')
-
-        if not a_side > 0:
-            raise ValueError(f'Side must be greater than 0. Got: {a_side}')
